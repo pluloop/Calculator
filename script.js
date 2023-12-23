a095d8a6e658fb0543be0e5969b6828b38e46c91
pluloop <andynguye852@gmail.com>
2023-12-23 00:50:21 -0800

    finished product

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const displayText = document.getElementById("display-text"); // to store expression
+
+// to perform action upon button click
 const button7 = document.getElementById("7");
 const button8 = document.getElementById("8");
 const button9 = document.getElementById("9");
@@ -12,30 +15,49 @@ const button3 = document.getElementById("3");
 const buttonMinus = document.getElementById("minus");
 const button0 = document.getElementById("0");
 const buttonDot = document.getElementById("dot");
-const buttonEqual = document.getElementById("equal");
-const buttonAdd = document.getElementById("add");
 const buttonCalculate = document.getElementById("calculate");
+const buttonAdd = document.getElementById("add");
+const buttonClear = document.getElementById("clear");
 
-var calculations = "";
+var result = ""; // value of expression
 
-function calculate(){
-    
+// calculates and display expression calculations 
+function evaluate(){
+    // if division by 0, result = ERROR
+    if (eval(result) === Infinity){
+        result = "ERROR";
+    }
+
+    // if @ least 1 operand is float, result = float
+    else if (result.includes(".")){
+        result = eval(result).toFixed(1);
+    }
+
+    // if both operands are int, result = int
+    else{
+        result = eval(result);
+    }
+
+    // displays calculations
+    displayText.innerText = result;
 }
 
-button7.addEventListener("click", function(){calculations += 7;});
-button7.addEventListener("click", function(){calculations += 8;});
-button7.addEventListener("click", function(){calculations += 9;});
-button7.addEventListener("click", function(){calculations += "/";});
-button7.addEventListener("click", function(){calculations += 4;});
-button7.addEventListener("click", function(){calculations += 5;});
-button7.addEventListener("click", function(){calculations += 6;});
-button7.addEventListener("click", function(){calculations += "*";});
-button7.addEventListener("click", function(){calculations += 1;});
-button7.addEventListener("click", function(){calculations += 2;});
-button7.addEventListener("click", function(){calculations += 3;});
-button7.addEventListener("click", function(){calculations += "-";});
-button7.addEventListener("click", function(){calculations += 0;});
-button7.addEventListener("click", function(){calculations += ".";});
-button7.addEventListener("click", function(){calculations += "=";});
-button7.addEventListener("click", function(){calculations += "+";});
-button7.addEventListener("click", calculate);
+// displays value of buttons upon click
+button7.addEventListener("click", function(){result += "7"; displayText.innerText = result;});
+button8.addEventListener("click", function(){result += "8"; displayText.innerText = result;});
+button9.addEventListener("click", function(){result += "9"; displayText.innerText = result;});
+buttonSlash.addEventListener("click", function(){result += "/"; displayText.innerText = result;});
+button4.addEventListener("click", function(){result += "4"; displayText.innerText = result;});
+button5.addEventListener("click", function(){result += "5"; displayText.innerText = result;});
+button6.addEventListener("click", function(){result += "6"; displayText.innerText = result;});
+buttonMultiply.addEventListener("click", function(){result += "*"; displayText.innerText = result;});
+button1.addEventListener("click", function(){result += "1"; displayText.innerText = result;});
+button2.addEventListener("click", function(){result += "2"; displayText.innerText = result;});
+button3.addEventListener("click", function(){result += "3"; displayText.innerText = result;});
+buttonMinus.addEventListener("click", function(){result += "-"; displayText.innerText = result;});
+button0.addEventListener("click", function(){result += "0"; displayText.innerText = result;});
+buttonDot.addEventListener("click", function(){result += "."; displayText.innerText = result;});
+buttonAdd.addEventListener("click", function(){result += "+"; displayText.innerText = result;});
+
+buttonCalculate.addEventListener("click", evaluate); // displays calculations upon click
+buttonClear.addEventListener("click", function(){result = ""; displayText.innerText = result;}); // clears display panel
